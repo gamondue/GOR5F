b297920b8ca6ed014a5cda345ff396a7a5688123
gamondue <DANIELE.PISCAGLIA@PC1308.itis.ce>
2015-02-10 12:49:47 +0100

    Aggiornamenti classe HIH4000

--- Programma_GOR_5F/GorDevices/RelativeHumidity_HIH4000.cs
+++ Programma_GOR_5F/GorDevices/RelativeHumidity_HIH4000.cs
@@ -95,9 +95,22 @@ namespace Gor.Devices
             //altro programma, che chiama i metodi di taratura del sensore
             //calibration = new Calibration_2Points(CalibrationFileName);
 
-            calibration = new Calibration_2Points();
+            /*calibration = new Calibration_2Points();
             calibration.AddPoint(0, 0);
-            calibration.AddPoint(4095, 100);
+            calibration.AddPoint(4095, 100);*/
+            try
+            {
+                if (CalibrationFileName != null)
+                    calibration = Calibration_2Points.Load(CalibrationFileName);
+            }
+            catch(Exception ex)
+            {
+                StartCalibration();//Nel metodo initialization bisogna calibrare il sensore se nonsi riesce a caricare una calibrazione?
+            }
+            
+            
+
+
 
         }
     }
