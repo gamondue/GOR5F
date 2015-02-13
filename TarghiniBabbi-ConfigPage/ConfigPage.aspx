41e5bc1a4c4326cb01446db8a375b59e7eb7a1b3
gamondue <gianluca.babbi@PC1316.itis.ce>
2015-02-13 09:53:59 +0100

    Targhini Babbi pagina di configurazione

---
+++ TarghiniBabbi-ConfigPage/ConfigPage.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ConfigPage.aspx.cs" Inherits="ConfigPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:CheckBox ID="chkTemperature" runat="server" Text="Temperatura" />
+        <br />
+        <asp:CheckBox ID="chkAirHumidity" runat="server" Text="Umidità" />
+        &nbsp;Dell&#39;Aria<br />
+        <asp:CheckBox ID="chkGroundHumidity" runat="server" Text="Umidità Terrena" />
+        <br />
+        <asp:CheckBox ID="chkPressure" runat="server" Text="Pressione" />
+        <br />
+        <asp:CheckBox ID="chkLux" runat="server" Text="Luminosità" />
+    
+    </div>
+        <p>
+        <asp:Button ID="btnSalva" runat="server" Text="Salva " OnClick="btnSalva_Click" Width="128px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ TarghiniBabbi-ConfigPage/ConfigPage.aspx.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Babbi
+ * Targhini
+ * 5°F
+ * Progetto Garden of Raspberries
+ * 13-02-15
+ * QUASI TERMINATO
+ */ 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class ConfigPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSalva_Click(object sender, EventArgs e)
+    {
+        string s = "";
+        if (chkTemperature.Checked == true)
+            s += "Temperature";
+        if (chkPressure.Checked == true)
+            s += "\r\n Pressure";
+        if (chkGroundHumidity.Checked == true)
+            s += "\r\n GroundHumidity";
+        if (chkAirHumidity.Checked == true)
+            s += "\r\n AirHumidity";
+        if (chkLux.Checked == true)
+            s += "\r\n Lux";
+
+        //Stringa da cambiare
+        using (StreamWriter sw = File.CreateText("C:\\Users\\gianluca.babbi\\Desktop\\Sensori Selezionati"))
+        {
+            sw.WriteLine(s);
+        }
+    }
+}
